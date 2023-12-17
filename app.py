d2cbaa84e2f20360b0c1c1549a629f616e741d18
Shun Isii <sisii00.swork@gmail.com>
2023-12-17 09:44:53 +0900

    なんかローカルにあったのコミット

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import aws_cdk as cdk
 
 app = cdk.App()
-stack = cdk.Stack(app, "{{test}}-stack")
+stack = cdk.Stack(app, "cdk-ec2-deploy-stack")
 
-# ここに必要なリソース書く
+VPC_CIDR = "10.111.0.0/16"
+vpc = cdk.aws_ec2.Vpc(
+    stack,
+    "VPC",
+    cidr=VPC_CIDR,
+    max_azs=1,
+)
+# デフォルトではnatgatewayが作られてkコストがかかる。
 
 app.synth()
