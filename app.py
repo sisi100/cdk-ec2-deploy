93cb707f2eedd3d397252b3db44ff7c3f4af4717
Shun Isii <sisii00.swork@gmail.com>
2023-12-17 11:23:05 +0900

    保存しとく

--- app.py
+++ app.py
@@ -4,12 +4,20 @@ app = cdk.App()
 stack = cdk.Stack(app, "cdk-ec2-deploy-stack")
 
 VPC_CIDR = "10.111.0.0/16"
+
 vpc = cdk.aws_ec2.Vpc(
     stack,
     "VPC",
     cidr=VPC_CIDR,
     max_azs=1,
+    subnet_configuration=[
+        cdk.aws_ec2.SubnetConfiguration(
+            name="Private",
+            cidr_mask=24,
+            subnet_type=cdk.aws_ec2.SubnetType.PUBLIC,
+        )
+    ],
 )
-# デフォルトではnatgatewayが作られてkコストがかかる。
+
 
 app.synth()
