7b6f07f17b978d9c3b1e390ee7e83f5229cf03ba
Shun Isii <sisii00.swork@gmail.com>
2024-02-11 23:52:00 +0900

    更新

--- app.py
+++ app.py
@@ -3,20 +3,31 @@ import aws_cdk as cdk
 app = cdk.App()
 stack = cdk.Stack(app, "cdk-ec2-deploy-stack")
 
-VPC_CIDR = "10.111.0.0/16"
 
+# VPC
+VPC_CIDR = "10.141.0.0/16"  # お好みで変更ください
 vpc = cdk.aws_ec2.Vpc(
     stack,
-    "VPC",
+    "vpc",
     cidr=VPC_CIDR,
     max_azs=1,
     subnet_configuration=[
         cdk.aws_ec2.SubnetConfiguration(
-            name="Private",
-            cidr_mask=24,
+            name="public",
             subnet_type=cdk.aws_ec2.SubnetType.PUBLIC,
-        )
+        ),
     ],
+)
+
+# EC2
+instance = cdk.aws_ec2.Instance(
+    stack,
+    "instance",
+    vpc=vpc,
+    vpc_subnets=cdk.aws_ec2.SubnetSelection(subnet_type=cdk.aws_ec2.SubnetType.PUBLIC),
+    instance_type=cdk.aws_ec2.InstanceType.of(cdk.aws_ec2.InstanceClass.T2, cdk.aws_ec2.InstanceSize.MICRO),
+    machine_image=cdk.aws_ec2.AmazonLinuxImage(generation=cdk.aws_ec2.AmazonLinuxGeneration.AMAZON_LINUX_2),
+    ssm_session_permissions=True,
 )
 
 
